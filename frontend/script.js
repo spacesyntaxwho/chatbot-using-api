0b516ddd14707de489c2244c72873b2bee87f2c2
Anuj <spacesyntax37@gmail.com>
2026-05-18 16:17:36 +0530

    Updated frontend UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,64 @@
 async function sendMessage() {
+
     let input = document.getElementById("input");
     let message = input.value;
 
-    let res = await fetch("http://127.0.0.1:5000/chat", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ message: message })
-    });
+    if(message.trim() === ""){
+        return;
+    }
 
-    let data = await res.json();
+    let chat = document.getElementById("chat");
 
-    document.getElementById("chat").innerHTML += 
-        "<p><b>You:</b> " + message + "</p>" +
-        "<p><b>Bot:</b> " + data.reply + "</p>";
+    // User Message
+    chat.innerHTML += `
+        <div class="message user">
+            <b>You:</b> ${message}
+        </div>
+    `;
+
+    chat.scrollTop = chat.scrollHeight;
+
+    try {
+
+        let res = await fetch("http://127.0.0.1:5000/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ message: message })
+        });
+
+        let data = await res.json();
+
+        // Bot Message
+        chat.innerHTML += `
+            <div class="message bot">
+                <b>Bot:</b> ${data.reply}
+            </div>
+        `;
+
+        chat.scrollTop = chat.scrollHeight;
+
+    } 
+    catch(error) {
+
+        chat.innerHTML += `
+            <div class="message bot">
+                <b>Bot:</b> Server Error
+            </div>
+        `;
+    }
 
     input.value = "";
 }
+
+// Enter key support
+
+document.getElementById("input")
+.addEventListener("keypress", function(event){
+
+    if(event.key === "Enter"){
+        sendMessage();
+    }
+
+});
